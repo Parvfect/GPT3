9cf4739e2c5f46fe159c125709fd004ed011ea72
Parvfect <parvagrw02@gmail.com>
2021-09-29 15:46:00 +0100

    First commit

---
+++ applications.py
@@ -0,0 +1,71 @@
+
+"""
+Copying all the useful GPT3 Application examples and testing them out 
+"""
+
+# https://beta.openai.com/examples
+
+
+def chat():
+
+def q&a():
+
+def grammar_check():
+
+def summarise_text():
+
+def openai_prompt():
+
+def text_to_command():
+
+def translate_text():
+
+def sql_query():
+
+def parse_data():
+
+
+def classification():
+
+def sentiment_analysis():
+
+def python_to_natual_language():
+
+def translate_programming_language():
+
+def explain_code():
+
+def keywords_from_text():
+
+def ad_from_description():
+
+def product_name_generator():
+
+def tldr():
+
+def javascript_helper_chatbot():
+
+def ml_helper_chatbot():
+
+def extract_contact_info():
+
+def friend_chat():
+
+def write_function_docstring():
+
+def analogy_maker():
+
+def compress_js_function():
+
+def third_person_converter():
+
+def summarise_meeting_notes():
+
+def essay_outline():
+
+def recipe_maker():
+
+def create_study_notes():
+
+def create_interview_questions():
+
